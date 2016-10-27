dd1e295e60433bb10fee297306fa5b9d334216d0
BAO Han <zsbcydyx@gmail.com>
2016-10-27 23:52:00 +0800

    backgammon game version1

---
+++ back/src/back/Game.java
@@ -0,0 +1,100 @@
+package back;
+
+import java.util.Random;
+import back.BoardDrawerCL;
+
+public class Game {
+	Random generator;
+	Board board;
+	Stone.Color player;
+	Dices dices;
+	int moveOrNot = 0;
+	
+	public Game(){
+		generator = new Random();
+		board = new Board();
+		player = Stone.Color.NONE;
+		dices = new Dices();
+	}
+	
+	public void roll(){
+		if(player == Stone.Color.NONE){
+			dices.rollDifferent();
+		}else{
+			dices.roll();
+			System.out.println(player + " Dices: " + dices.getDiceOne() + " " + dices.getDiceTwo() );
+		}
+		
+		switch(player){
+		case WHITE:
+			player = Stone.Color.BLACK;
+			break;
+		case BLACK:
+			player = Stone.Color.WHITE;
+			break;
+		case NONE:
+			if(dices.getDiceOne() > dices.getDiceTwo()){
+				player = Stone.Color.WHITE;
+			}else{
+				player = Stone.Color.BLACK;
+			}
+			break;
+		}
+		
+	}
+	
+
+	public boolean canMove(int from, int count){
+		if(!dices.isRolled()) return false;
+		if(!dices.isOnDice(count)) return false;
+		if(!board.canMove(from, count)) return false;
+		if(board.getStone(from).color() == player) return false;
+		return true;
+	}
+	
+	public void move(int from, int count) throws WrongMoveException{
+		if(!canMove(from,count)) throw new WrongMoveException();
+		board.move(from,count);
+		dices.takeDice(count);
+		moveOrNot = 1;
+		BoardDrawerCL.draw(board);
+	}
+/*	
+	public boolean canPut(int number){
+		if(!dices.isRolled()) return false;
+		if(!dices.isOnDice(number)) return false;
+		if(!board.canPut(player, number)) return false;
+		
+		return true;
+	}
+	
+	public void put(int number) throws WrongMoveException{
+		if(!canPut(number)) throw new WrongMoveException();
+		board.put(player,number);
+		dices.takeDice(number);
+		moveOrNot = 0;
+		BoardDrawerCL.draw(board);
+	}
+*/
+	public Board getBoard(){
+		return board;
+	}
+	
+	public Stone.Color getPlayer(){
+		return player;
+	}
+	
+	public ShowOnlyDices getDice(){
+		return new ShowOnlyDices(dices);
+	}
+	
+	public boolean isEnded(){		
+		return board.getHome(Stone.Color.WHITE) == 15 || board.getHome(Stone.Color.BLACK) == 15;
+	}
+	
+	public Stone.Color winner(){
+		if(!isEnded()) return Stone.Color.NONE;
+		if(board.getHome(Stone.Color.WHITE) == 15) return Stone.Color.WHITE;
+		return Stone.Color.BLACK;
+	}
+}
---
+++ back/src/back/backgammon.java
@@ -0,0 +1,48 @@
+package back;
+
+import back.Game;
+import java.util.Scanner;
+
+public class backgammon {
+	private static Scanner sc;
+
+	public static void main(String argv[]){
+		Game g = new Game();
+		int move,movedic;
+		g.roll();
+		while(g.winner() == Stone.Color.NONE){
+			g.roll();
+			while(g.dices.isRolled()){
+			System.out.println("Please enter piece location using blankspace(like a b, a is the numer, b is move number): ");
+			sc = new Scanner(System.in);
+			String input = sc.nextLine();
+			if(input.split(" ").length == 2){
+				move = Integer.parseInt(input.split(" ")[0]);
+				movedic = Integer.parseInt(input.split(" ")[1]);
+				if(g.getBoard().getBarCount(Stone.Color.BLACK) > 0){
+					
+				}
+				if(g.getBoard().getBarCount(Stone.Color.WHITE) > 0){
+					
+				}
+				if(g.canMove(move-1, movedic)){
+					if(movedic == g.dices.getDiceOne() || movedic == g.dices.getDiceTwo()){
+						try {	
+							g.move(move-1,movedic);
+						} catch (WrongMoveException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				else{
+					System.out.println("Error move!");
+				}
+			}
+			
+			}
+		}
+		System.out.println("Congratulations!" + g.winner() + " is the winner!");
+
+	}
+}
